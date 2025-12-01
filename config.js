10fedd03fb38a5e1125de91a2763193b1ee8abae
ShonkuWeb <shonkuweb@ShonkuWebs-Mac-mini.local>
2025-12-01 20:06:42 +0530

    m

--- config.js
+++ config.js
@@ -7,13 +7,7 @@ const CONFIG = {
         "l": { amount: 2, code: "3333", image: "terms_content.png", name: "shibam" },
         "jit": { amount: 5000, code: "2321", image: "terms_content.png", name: "ma manasa" },
         "m": { amount: 400000, code: "0000", image: "terms_content.png", name: "elon musk" },
-        "u1": { "amount": 1500, code: "8273", image: "terms_content.png", name: "arjun" },
-       
-}
-
-
-
-
+        "u1": { amount: 1500, code: "8273", image: "terms_content.png", name: "arjun" }
     },
 
     // You can add other global settings here in the future
